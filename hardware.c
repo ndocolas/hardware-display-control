544c67ca778082a84f8a5a38fa0ad16265329ea5
Nicolas <salocod@icloud.com>
2024-05-24 18:13:34 -0300

    implementacao de varias correcoes: nome dos metodos alterados, estrutura corrigida

--- hardware.c
+++ hardware.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 #include "hardware.h"
 
-void set_display_state(unsigned short *r0, int mode) {
-    if(mode == 1) {
-        *r0 |= (1 << 0);
-    } else if(mode == 0){
-        *r0 &= ~(1 << 0);
-    }
-}
+void def_display_state(unsigned short *r0, int value) {
+    (value == 1) ? (*r0 |= (1 << 0)) : (*r0 &= ~(1 << 0));}
 
-void set_display_mode(unsigned short *r0, int mode) {
+void def_display_mode(unsigned short *r0, int mode) {
     if (mode < 0 || mode > 3) {
         printf("Erro: Modo display inválido. O valor deve ser entre 0 e 3.\n");
         return;
@@ -20,7 +15,7 @@ void set_display_mode(unsigned short *r0, int mode) {
     *r0 |= (mode << 1);
 }
 
-void set_refresh_rate(unsigned short *r0, int value) {
+void def_refresh_rate(unsigned short *r0, int value) {
     if (value < 0 || value > 63) {
         printf("Erro: %d Valor inválido. O valor deve ser entre 0 e 63.\n", value);
         return;
@@ -29,27 +24,23 @@ void set_refresh_rate(unsigned short *r0, int value) {
     *r0 |= (value << 3);
 }
 
-void set_led_operation_state(unsigned short *r0, int state) {
-    if(state == 1) {
-        *r0 |= (1 << 9);
-    } else {
-        *r0 &= ~(1 << 9);
-    }
+void det_led_operation_state(unsigned short *r0, int state) {
+    (state == 1) ? (*r0 |= (1 << 9)) : (*r0 &= ~(1 << 9));
 }
 
-void set_led_color(unsigned short *r0, int value, int bit) {*r0 |= (value << bit);}
+void def_led_color_value(unsigned short *r0, int value, int bit) {*r0 |= (value << bit);}
 
 void def_led_color(unsigned short *r0, int value) {
-    *r0 &= ~(7 << 10);
-    switch(value) {
-        case 0: set_led_color(r0, 1, 12);
-        case 1: set_led_color(r0, 3, 11);
-        case 2: set_led_color(r0, 1, 11);
-        case 3: set_led_color(r0, 1, 11);
-    }
+        *r0 &= ~(7 << 10);
+        switch(value) {
+            case 0: *r0 |= (1 << 12);break;
+            case 1: *r0 |= (3 << 11);break;
+            case 2: *r0 |= (1 << 11);break;
+            case 3: *r0 |= (1 << 11);break;
+        }
 }
 
-void activate_default_state(unsigned short *r0) {*r0 |= (1 << 9);}
+void restore_deafult(unsigned short *r0) {*r0 |= (1 << 13);}
 
 void def_color_red(unsigned short *r1, int value) {
     *r1 &= ~(255 << 0);
@@ -83,21 +74,25 @@ void def_word(unsigned short *r[], char word[]) {
 
         int ascii_value = word[word_index];
         
-        *r[register_index] |= (ascii_value << 0); // Seta o byte de baixa ordem diretamente
-
+        *r[register_index] |= (ascii_value << 0);
+        
         ascii_value = word[word_index++];
 
-        *r[register_index] |= (ascii_value << 8); // Limpa o byte de alta ordem
+        *r[register_index] |= (ascii_value << 8);
         
-        word_index++; // Incrementa por 2 para processar os caracteres em pares
+        word_index+=1;
         register_index++;
     }
 }
 
-char* read_status_display(unsigned short *r0) {return (((*r0 >> 0) & 0b1) == 0) ? "OFF" : "ON";}
+int read_display_state_value(unsigned short *r0) {return ((*r0 >> 0) & 0b1) == 0;}
+
+char* read_display_state(unsigned short *r0) {return read_display_state_value(*r0) ? "OFF" : "ON";}
+
+int read_display_mode_value(unsigned short *r0) {return ((*r0 >> 1) & 0b11);}
 
 char* read_display_mode(unsigned short *r0) {
-    switch((*r0 >> 1) & 0b11) {
+    switch(read_display_mode_value(*r0)) {
         case 0: return "Estatico";
         case 1: return "Deslizante";
         case 2: return "Piscante";
@@ -108,12 +103,19 @@ char* read_display_mode(unsigned short *r0) {
 
 int read_refresh_rate(unsigned short *r0) {return ((*r0 >> 3) & 0b111111);}
 
-char* read_led_operation(unsigned short *r0) {return (((*r0 >> 9) & 0b1) == 0) ? "OFF" : "ON";}
+int read_led_operation_value(unsigned short *r0) {return ((*r0 >> 9) & 0b1) == 0;}
+char* read_led_operation(unsigned short *r0) {return read_led_operation_value(*r0) ? "OFF" : "ON";}
 
-char* read_color_led(unsigned short *r0) {
+int read_color_display_red(unsigned short *r1) {return((*r1 >> 0) & 0b11111111);}
+int read_color_display_green(unsigned short *r1) {return((*r1 >> 8) & 0b11111111);}
+int read_color_display_blue(unsigned short *r2) {return((*r2 >> 10) & 0b11111111);}
+
+char* read_color_display(unsigned short *r1, unsigned short *r2) {
     static char result[64];
     snprintf(result, sizeof(result), "Valor:\n [R] : %d\n [G] : %d\n [B] : %d", 
-             ((*r0 >> 12) & 0b1), ((*r0 >> 11) & 0b1), ((*r0 >> 10) & 0b1));
+             read_color_display_red(*r1), 
+             read_color_display_green(*r1), 
+             read_color_display_blue(*r2));
     return result;
 }
 
@@ -158,25 +160,25 @@ void run_program(unsigned short *registers[]) {
             case 1:
                 printf("\nLigar/Desligar display: \n [0] Desligar\n [1] Ligar\n");
                 scanf("%d", &sub);
-                if (sub == 1 || sub == 0) set_display_state(registers[0], sub);
+                if (sub == 1 || sub == 0) def_display_state(registers[0], sub);
                 else printf("Escolha inválida. Tente novamente.\n");
                 break;
             case 2:
                 printf("\nModo de Exibicao:\n [0] Estatico\n [1] Deslizante\n [2] Piscante\n [3] Deslizante/Piscante\n");
                 scanf("%d", &sub);
-                if(sub >= 0 && sub <= 3) set_display_mode(registers[0], sub);
+                if(sub >= 0 && sub <= 3) def_display_mode(registers[0], sub);
                 else printf("Escolha inexistente. Tente novamente.\n");
                 break;
             case 3:
                 printf("\nRefresh Rate: \n\n Insira um valor entre 0 e 63!\nvalor: ");
                 scanf("%d", &sub);
-                if(sub >= 0 && sub <= 63) set_refresh_rate(registers[0], sub);
+                if(sub >= 0 && sub <= 63) def_refresh_rate(registers[0], sub);
                 else printf("Valor invalido. Tente novamente.\n");
                 break;
             case 4:
                 printf("\nLigar/Desligar: \n [0] Desligar\n [1] Ligar\n");
                 scanf("%d", &sub);
-                if(sub <= 1 && sub >= 0) set_led_operation_state(registers[0], sub);
+                if(sub <= 1 && sub >= 0) det_led_operation_state(registers[0], sub);
                 else printf("Valor invalido. Tente novamente.\n");
                 break;
             case 5:
@@ -216,7 +218,7 @@ void run_program(unsigned short *registers[]) {
             case 7:
                 printf("\nRestaurar Padrao: \nVoce deseja restaurar para o padrao de fabrica? \n [0] Nao \n [1] Sim\n");
                 scanf("%d", &sub);
-                if(sub == 1) activate_default_state(registers[0]);
+                if(sub == 1) restore_deafult(registers[0]);
                 break;
             case 8:
                 while(1) {
@@ -228,11 +230,11 @@ void run_program(unsigned short *registers[]) {
                     scanf("%d", &sub_case7);
                     if(sub_case7 == 0) break;
                     switch(sub_case7) {
-                        case 1: printf("\nDisplay: %s\n\n", read_status_display(registers[0])); break;
+                        case 1: printf("\nDisplay: %s\n\n", read_display_state(registers[0])); break;
                         case 2: printf("\nModo display: %s\n\n", read_display_mode(registers[0])); break;
                         case 3: printf("\nValor do refresh rate: %d\n\n", read_refresh_rate(registers[0])); break;
                         case 4: printf("\nValor da led: %s\n\n", read_led_operation(registers[0])); break;
-                        case 5: printf("\nValor das cores: %s\n\n", read_color_led(registers[0])); break;
+                        case 5: printf("\nValor das cores: %s\n\n", read_color_display(registers[1], registers[2])); break;
                         case 6: printf("\nNivel da bateria: %s\n\n", read_battery_status(registers[3])); break;
                         case 7: printf("\nContagem de passagens no modo deslizante: %d\n\n", read_number_of_times_screen(registers[3])); break;
                         case 8: printf("\nTemperatura atual: %d\n\n", read_temperature(registers[3])); break;
--- hardware.h
+++ hardware.h
@@ -1,27 +1,54 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
-void set_display_state(unsigned short *r0, int mode);
-void set_display_mode(unsigned short *r0, int mode);
-void set_refresh_rate(unsigned short *r0, int value);
-void set_led_operation_state(unsigned short *r0, int state);
-void set_led_color(unsigned short *r0, int bit, int value);
-void def_led_color(unsigned short *r0, int value);
-void activate_default_state(unsigned short *r0);
+
+//1
+void def_display_state(unsigned short *r0, int mode);
+//2
+void def_display_mode(unsigned short *r0, int mode);
+//3
+void def_refresh_rate(unsigned short *r0, int value);
+//4
+void det_led_operation_state(unsigned short *r0, int state);
+//5
 void def_color_red(unsigned short *r1, int value);
 void def_color_green(unsigned short *r1, int value);
 void def_color_blue(unsigned short *r2, int value);
+//6
 void def_word(unsigned short *r[], char word[]);
+//8
+void restore_deafult(unsigned short *r0);
+//manipulacao automatica
+void def_led_color_value(unsigned short *r0, int bit, int value);
+void def_led_color(unsigned short *r0, int value);
 
-char* read_status_display(unsigned short *r0);
+
+//LER------------------------
+
+//1
+int read_display_state_value(unsigned short *r0);
+char* read_display_state(unsigned short *r0);
+//2
+int read_display_mode_value(unsigned short *r0);
 char* read_display_mode(unsigned short *r0);
+//3
 int read_refresh_rate(unsigned short *r0);
+//4
+int read_led_operation_value(unsigned short *r0);
 char* read_led_operation(unsigned short *r0);
-char* read_color_led(unsigned short *r0);
+//5
+int read_color_display_red(unsigned short *r1);
+int read_color_display_green(unsigned short *r1);
+int read_color_display_blue(unsigned short *r2);
+char* read_color_display(unsigned short *r1, unsigned short *r2);
+//6
 int read_battery_status_int(unsigned short *r3);
 char* read_battery_status(unsigned short *r3);
+//7
 int read_number_of_times_screen(unsigned short *r3);
+//8
 int read_temperature(unsigned short *r3);
+//9
 void read_word(unsigned short *r[]);
 
 void run_program(unsigned short *registers[]);
